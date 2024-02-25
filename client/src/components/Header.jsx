c9e7361917730a3c14b8cea8283ae0955b3f983f
Alphatech29 <alphatechmultimedia@gmail.com>
2024-02-25 10:10:51 +0100

    Haeder code add

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Navbar } from 'flowbite-react';
 
 export default function Header() {
+    
   return (
-    <div>
-      Hearder
-    </div>
+  <Navbar className='sticky left-0 right-0 top-0'>
+     <Navbar.Brand href="/">
+        <img src="image/favicon.png" className="mr-3 h-6 sm:h-9" alt="Alphatech Logo" />
+      </Navbar.Brand>
+      <Navbar.Collapse >
+        <Navbar.Link href="/" active>Home</Navbar.Link>
+        <Navbar.Link href="/portfolio">Portfolio</Navbar.Link>
+        <Navbar.Link href="/Services">Services</Navbar.Link>
+        <Navbar.Link href="/bootcamp">Bootcamp</Navbar.Link>
+        <Navbar.Link href="/about">About Us</Navbar.Link>
+      </Navbar.Collapse>
+  </Navbar>
+
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Home() {
+  
   return (
     <div>
       <h1>Alphatech React</h1>
