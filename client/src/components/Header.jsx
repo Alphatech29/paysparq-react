ff6ee2d60d5eb27e97d8a6bbbe9fb7e897c64c28
Alphatech29 <alphatechmultimedia@gmail.com>
2024-03-01 18:59:31 +0100

    React-Boostrap added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Navbar } from 'flowbite-react';
+import { Navbar } from 'flowbite-react'
+
 
 export default function Header() {
     
   return (
-  <Navbar className='sticky left-0 right-0 top-0'>
+  <Navbar className='sticky left-0 right-0 top-0 sm:px-12'>
      <Navbar.Brand href="/">
-        <img src="image/favicon.png" className="mr-3 h-6 sm:h-9" alt="Alphatech Logo" />
+     <img src="/client/assets/image/favicon.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
       </Navbar.Brand>
       <Navbar.Collapse >
         <Navbar.Link href="/" active>Home</Navbar.Link>
